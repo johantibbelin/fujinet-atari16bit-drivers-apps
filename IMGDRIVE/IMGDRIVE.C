a19bcc5d7dacac80b0b43142b3e97b2be6be71d4
Johan Tibbelin <42235680+johantibbelin@users.noreply.github.com>
2025-03-31 23:36:47 +0200

    Start of IMGDRIVE

---
+++ IMGDRIVE/IMGDRIVE.C
@@ -0,0 +1,25 @@
+/**
+ * IMGDRIVE - Mount disk images to A or B drive
+ *
+ * Written by: Johan Tibbelin (sjfroos)
+ * 
+ * for the Fujinet project
+ *
+ * Start date: 2025-03-31
+ *
+ * Last changed: 2025-03-31
+ *
+ * Version: 0.01a
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <tos.h>
+#include <aes.h>
+#include "a16defs.h"
+#include "imgrwabs.h"
+
+int main() {
+
+	return 0;
+}
